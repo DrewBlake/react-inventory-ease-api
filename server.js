aba399847d38b09faebe1b43f26617e855945e1e
Andrew Blake <drewblake1974@gmail.com>
2018-11-18 14:38:15 -0500

    updated travis to deploy heroku

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
- const app = express();
+const app = express();
+const cors = require('cors');
+const {CLIENT_ORIGIN} = require('./config');
 
- const PORT = process.env.PORT || 3000;
+app.use(
+    cors({
+        origin: CLIENT_ORIGIN
+    })
+);
 
- app.get('/api/*', (req, res) => {
-   res.json({ok: true});
- });
+const PORT = process.env.PORT || 3000;
 
- app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.get('/api/*', (req, res) => {
+res.json({ok: true});
+});
 
- module.exports = {app};
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+module.exports = {app};
